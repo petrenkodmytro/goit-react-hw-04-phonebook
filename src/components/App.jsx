977a17636809db862fac6d676675a6f99801e296
Dmytro Petrenko <petrenko_dmytro@ukr.net>
2023-05-02 22:18:44 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useEffect, useState } from 'react';
 import { GlobalStyle } from './GlobalStyle';
 import { Layout } from './Layout/Layout';
 import { ContactList } from './ContactList/ContactList';
@@ -14,38 +14,42 @@ import {
 
 // render > didMount > getItem > setState > update > render > didUpdate > setItem
 
-export class App extends Component {
-  state = {
-    contacts: [],
-    filter: '',
-  };
+export const App = () => {
+  const [contacts, setContacts] = useState([]);
+  const [filter, setFilter] = useState('');
 
   // стадія монтування
-  componentDidMount() {
-    const savedContacts = localStorage.getItem('contacts');
-    // Если сохранили в LS уже что-то, пишем ЭТО в state
-    if (savedContacts !== null) {
-      const parsedContacts = JSON.parse(savedContacts);
-      this.setState({ contacts: parsedContacts });
-      return;
-    }
-    // Если в LS ничего еще нет, пишем в state initialRecipes
-    this.setState({ contacts: initialContacts });
-  }
+  // componentDidMount() {
+  //   const savedContacts = localStorage.getItem('contacts');
+  //   // Если сохранили в LS уже что-то, пишем ЭТО в state
+  //   if (savedContacts !== null) {
+  //     const parsedContacts = JSON.parse(savedContacts);
+  //     this.setState({ contacts: parsedContacts });
+  //     return;
+  //   }
+  //   // Если в LS ничего еще нет, пишем в state initialRecipes
+  //   this.setState({ contacts: initialContacts });
+  // }
 
-  // стадія оновлення
-  componentDidUpdate(prevProps, prevState) {
-    if (this.state.contacts !== prevState.contacts) {
-      // console.log(this.state.contacts); // поточне значення
-      // console.log(prevState.contacts); // попереднє значення
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
-  }
+  // // стадія оновлення
+  // componentDidUpdate(prevProps, prevState) {
+  //   if (this.state.contacts !== prevState.contacts) {
+  //     // console.log(this.state.contacts); // поточне значення
+  //     // console.log(prevState.contacts); // попереднє значення
+  //     localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+  //   }
+  // }
 
-  addContact = newContact => {
+  useEffect(() => {
+    // массив зависимости - это под капотом оператор if
+    //if (this.state.contacts !== prevState.contacts)
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  const addContact = newContact => {
     // перевірка на існуюче ім'я контакту
     if (
-      this.state.contacts.some(
+      contacts.some(
         contact =>
           contact.name.toLocaleLowerCase() ===
           newContact.name.toLocaleLowerCase()
@@ -59,46 +63,40 @@ export class App extends Component {
       return;
     }
     // додавання нового контакту
-    this.setState(prevState => ({
-      contacts: [...prevState.contacts, newContact],
-    }));
+    setContacts(prevState => [...prevState, newContact]);
   };
 
-  deleteContact = contactId => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
-    }));
-  };
-
-  changeFilter = e => {
-    this.setState({ filter: e.currentTarget.value });
-  };
-
-  render() {
-    const normalizeFilter = this.state.filter.toLocaleLowerCase();
-    const visibleContacts = this.state.contacts
-      .filter(contact =>
-        contact.name.toLocaleLowerCase().includes(normalizeFilter)
-      )
-      .sort((firstName, secondName) =>
-        firstName.name.localeCompare(secondName.name)
-      );
-
-    return (
-      <Layout>
-
-        <h1>Phonebook</h1>
-        <ContactForm onSave={this.addContact} />
-
-        <h2>Contacts</h2>
-        <Filter value={this.state.filter} onSearch={this.changeFilter} />
-        
-        <ContactList items={visibleContacts} onDelete={this.deleteContact} />
-
-        <ToastContainer />
-        <GlobalStyle />
-
-      </Layout>
+  const deleteContact = contactId => {
+    setContacts(prevState =>
+      prevState.filter(contact => contact.id !== contactId)
     );
-  }
-}
+  };
+
+  const changeFilter = e => {
+    setFilter(e.currentTarget.value);
+  };
+
+  const normalizeFilter = filter.toLocaleLowerCase();
+  const visibleContacts = contacts
+    .filter(contact =>
+      contact.name.toLocaleLowerCase().includes(normalizeFilter)
+    )
+    .sort((firstName, secondName) =>
+      firstName.name.localeCompare(secondName.name)
+    );
+
+  return (
+    <Layout>
+      <h1>Phonebook</h1>
+      <ContactForm onSave={addContact} />
+
+      <h2>Contacts</h2>
+      <Filter value={filter} onSearch={changeFilter} />
+
+      <ContactList items={visibleContacts} onDelete={deleteContact} />
+
+      <ToastContainer />
+      <GlobalStyle />
+    </Layout>
+  );
+};
